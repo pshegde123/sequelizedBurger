4e34dcfd84394b71208ac878fe805ae07cf5cb89
pradnya <pradnyahegde@gmail.com>
2019-12-02 17:31:24 -0500

    updated validation option

--- models/burger.js
+++ models/burger.js
@@ -1,16 +1,22 @@
-module.exports = function(sequelize,DataTypes){
-var Burger = sequelize.define("Burger",{
-    burger_name:{
-        type:DataTypes.STRING,
-        allowNull:false,
-        validate:{
-            len:[1]
-        }
-    },
-    devoured:{
-        type:DataTypes.BOOLEAN,
-        defaultValue:false
-    }
-});
+module.exports = function (sequelize, DataTypes) {
+    var Burger = sequelize.define("Burger", {
+        Name: { type: DataTypes.STRING, 
+            allowNull: false,
+            validate:{len:[1],
+            is: /[\w ]/g  // A-Z, a-z, 0-9, or a space
+            }
+         },
+        Devoured: { type: DataTypes.BOOLEAN, defaultValue: false }
+    }, {
+        timestamps: false
+    });
+    Burger.associate = function (models) {
+        Burger.belongsTo(models.Customer, {
+            onDelete: "CASCADE",
+            foreignKey: {
+                allowNull: true
+            }
+        })
+    };
     return Burger;
 };
