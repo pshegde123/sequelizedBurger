49b048ba6ed752afc475d3c9a4c070ef31409883
pradnya <pradnyahegde@gmail.com>
2019-12-02 17:33:25 -0500

    Updated route queries to include Customer table

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,28 +6,38 @@ var router = express.Router();
 var db = require("../models");
 
 router.get("/", function (req, res) {
-    //console.log("************ Here ************");
-    //console.log("req=",req.body);
-    db.Burger.findAll({}).then(function (data) {
+    db.Burger.findAll({
+        include: [{ model: db.Customer }],
+        order: [['Name']]
+    }).then(function (data) {
         res.render("index", { burgers: data });
     });
 });
 
 router.post("/burgers", function (req, res) {
-    db.Burger.create(req.body).then(function(result){
-    res.json(result);
+    db.Burger.create(req.body).then(function (result) {
+        res.json(result);
     });
 });
 
 router.put("/burger:id", function (req, res) {
-    db.Burger.update({devoured:true},{
-        where:{
-            id:req.params.id.slice(1)
-        }
-    }).then(function(result){
-        res.json(result);
-    });
-})
+    console.log("controller:req.body=", req.body);
+    db.Customer.findOrCreate({
+        where: { Name: req.body.customer_name }
+    }).spread((customer) => {
+        customer.get({ plain: true });
+        db.Burger.update(
+            {
+                Devoured: true,
+                CustomerId: customer.id
+            }, 
+            { 
+                where: { id: req.params.id.slice(1) } 
+            }).then(function (dbBurger) {
+                res.json(dbBurger);
+            })
+    })
+});
 
 // Export routes for server.js to use.
 module.exports = router;
