c56df0953f4350e51401f94be6fb637d0ee64c36
pradnya <pradnyahegde@gmail.com>
2019-12-02 18:06:37 -0500

    removed validation option for heroku error

--- models/burger.js
+++ models/burger.js
@@ -1,10 +1,7 @@
 module.exports = function (sequelize, DataTypes) {
     var Burger = sequelize.define("Burger", {
         Name: { type: DataTypes.STRING, 
-            allowNull: false,
-            validate:{len:[1],
-            is: /[\w ]/g  // A-Z, a-z, 0-9, or a space
-            }
+            allowNull: false
          },
         Devoured: { type: DataTypes.BOOLEAN, defaultValue: false }
     }, {
--- models/customer.js
+++ models/customer.js
@@ -2,10 +2,7 @@ module.exports = function (sequelize, DataTypes) {
     var Customer = sequelize.define("Customer", {
         Name: {
             type: DataTypes.STRING, 
-            allowNull: false, 
-            validate: { len: [1, 64],  
-            is: /[\w ]/g  // A-Z, a-z, 0-9, or a space 
-            }
+            allowNull: false
         }
         },{
         timestamps: false
