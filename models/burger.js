7898f599d364a564c761c47124d37d5f818b49be
pradnya <pradnyahegde@gmail.com>
2019-12-03 10:35:49 -0500

    final version

--- models/burger.js
+++ models/burger.js
@@ -1,19 +1,17 @@
-module.exports = function (sequelize, DataTypes) {
-    var Burger = sequelize.define("Burger", {
-        Name: { type: DataTypes.STRING, 
-            allowNull: false
-         },
-        Devoured: { type: DataTypes.BOOLEAN, defaultValue: false }
-    }, {
+module.exports = function(sequelize,DataTypes){
+    var Burger = sequelize.define("Burger",{
+        Name: { type: DataTypes.STRING, allowNull: false },
+        Devoured: { type: DataTypes.BOOLEAN, defaultValue: 0 }
+    },{
         timestamps: false
     });
-    Burger.associate = function (models) {
-        Burger.belongsTo(models.Customer, {
+    Burger.associate = function(models){
+        Burger.belongsTo(models.Customer,{
             onDelete: "CASCADE",
             foreignKey: {
-                allowNull: true
+              allowNull: true
             }
-        })
+        });
     };
     return Burger;
 };
