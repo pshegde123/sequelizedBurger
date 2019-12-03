3da13ba14ecf74130515bce54f1cbedffc054628
pradnya <pradnyahegde@gmail.com>
2019-12-03 10:34:44 -0500

    updated get,post and put routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,43 +1,32 @@
-var express = require("express");
-
-var router = express.Router();
-
-//import data model 
-var db = require("../models");
-
-router.get("/", function (req, res) {
-    db.Burger.findAll({
-        include: [{ model: db.Customer }],
-        order: [['Name']]
-    }).then(function (data) {
-        res.render("index", { burgers: data });
+// Global
+const express = require("express");
+const router = express.Router();
+const db = require("../models");
+// Default the route to /burgers (Main Home Page)
+router.get('/',function(req,res){
+    res.redirect("/burgers");
+});
+// Get Burgers 
+router.get('/burgers',function(req,res){
+    db.Burger.findAll({ include:[{model: db.Customer}],order: [ ['Name']]}).then(function(dbBurger){
+        var hbsObject = { burgers: dbBurger };
+        res.render('index',hbsObject);
     });
 });
-
-router.post("/burgers", function (req, res) {
-    db.Burger.create(req.body).then(function (result) {
-        res.json(result);
+// Create Burger
+router.post("/burgers/create",function(req,res){
+    db.Burger.create({Name: req.body.burger_name}).then(function(dbBurger){
+        res.redirect('/burgers');
     });
 });
-
-router.put("/burger:id", function (req, res) {
-    console.log("controller:req.body=", req.body);
-    db.Customer.findOrCreate({
-        where: { Name: req.body.customer_name }
-    }).spread((customer) => {
-        customer.get({ plain: true });
-        db.Burger.update(
-            {
-                Devoured: true,
-                CustomerId: customer.id
-            }, 
-            { 
-                where: { id: req.params.id.slice(1) } 
-            }).then(function (dbBurger) {
-                res.json(dbBurger);
-            })
+// Update Burger
+router.put('/burgers/update/:burger_id',function(req,res){
+    db.Customer.findOrCreate({where: {Name: req.body.customer_name}}).spread((customer) => {
+        customer.get({ plain: true});
+        db.Burger.update({Devoured: 1, CustomerId: customer.id},{where: {id: req.params.burger_id}}).then(function(dbBurger){
+            res.redirect('/burgers');
+        })
     })
 });
-
-// Export routes for server.js to use.
+// Export Router
 module.exports = router;
